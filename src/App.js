3b54bc3f7d1abab327cb66ffa3d79a1158afac63
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-25 21:12:01 +0100

    Added CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AppBar from './components/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <AppBar />
     </div>
   );

7f87b1dff218f966364a758f6113b062b409ac6d
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-08-26 16:06:12 +0100

    Post card created with material Complex Interaction card component

--- src/pages/AllPosts/index.js
+++ src/pages/AllPosts/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './Nav';
 import AppFilterMenu from '../../components/AppFilterMenu';
+import PostCard from '../../components/PostCard';
 import './style.css';
 
 export default function Index() {
@@ -9,6 +10,7 @@ export default function Index() {
       <Nav />
       <div className="main-page-container">
         <AppFilterMenu />
+        <PostCard />
       </div>
     </React.Fragment>
   );

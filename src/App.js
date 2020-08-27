ca3c7f7bacfc23533f7446a2f454d2739616c025
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-27 20:16:42 +0100

    Added fab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import AppBar from './components/AppBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import { CssBaseline } from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
+import { CssBaseline, Fab } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2)
+  }
+}));
 
 export default function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <CssBaseline />
@@ -13,6 +25,15 @@ export default function App() {
         <Switch>
           <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
+        <Fab
+          component={Link}
+          to="/post/new"
+          className={classes.fab}
+          color="secondary"
+          aria-label="New Post"
+        >
+          <EditIcon />
+        </Fab>
       </Router>
     </div>
   );

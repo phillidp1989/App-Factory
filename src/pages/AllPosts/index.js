2ddb992298fdeafcb2cda3be5af7e262b9c44415
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-08-29 13:11:49 +0100

    postCard max width changed to vw

--- src/pages/AllPosts/index.js
+++ src/pages/AllPosts/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import Nav from './Nav';
+// import Nav from './Nav';
 import AppFilterMenu from '../../components/AppFilterMenu';
 import PostCard from '../../components/PostCard';
+// import AppBar from '../../components/AppBar';
 import './style.css';
 
 export default function Index() {
   return (
     <React.Fragment>
-      <Nav />
-      <div className="main-page-container">
-        <AppFilterMenu />
-        <PostCard />
-      </div>
+      <AppFilterMenu />
+      <PostCard />
     </React.Fragment>
   );
 }

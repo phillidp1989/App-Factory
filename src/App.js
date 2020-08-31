ac36fd97095987b4fe64feaabb7716cb0d9f0f2c
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-31 21:51:28 +0100

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppBar from './components/AppBar';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import NewPost from './pages/NewPost';

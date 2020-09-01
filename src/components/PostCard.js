2de938b6577818be4bd041a23ef5df403f6fcf54
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-09-01 10:33:58 +0100

    grid try

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,110 +1,180 @@
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import clsx from 'clsx';
+// import Card from '@material-ui/core/Card';
+// import CardHeader from '@material-ui/core/CardHeader';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardActions from '@material-ui/core/CardActions';
+// import Collapse from '@material-ui/core/Collapse';
+// import Avatar from '@material-ui/core/Avatar';
+// import IconButton from '@material-ui/core/IconButton';
+// import Typography from '@material-ui/core/Typography';
+// import { red } from '@material-ui/core/colors';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+// import ShareIcon from '@material-ui/icons/Share';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     maxWidth: '70vw'
+//   },
+//   media: {
+//     height: 0,
+//     paddingTop: '56.25%' // 16:9
+//   },
+//   expand: {
+//     transform: 'rotate(0deg)',
+//     marginLeft: 'auto',
+//     transition: theme.transitions.create('transform', {
+//       duration: theme.transitions.duration.shortest
+//     })
+//   },
+//   expandOpen: {
+//     transform: 'rotate(180deg)'
+//   },
+//   avatar: {
+//     backgroundColor: red[500]
+//   }
+// }));
+
+// export default function PostCard() {
+//   const classes = useStyles();
+//   const [expanded, setExpanded] = React.useState(false);
+
+//   const handleExpandClick = () => {
+//     setExpanded(!expanded);
+//   };
+
+//   return (
+//     <Card className={classes.root}>
+//       <CardHeader
+//         avatar={
+//           <Avatar aria-label="post" className={classes.avatar}>
+//             P
+//           </Avatar>
+//         }
+//         action={
+//           <IconButton aria-label="settings">
+//             <MoreVertIcon />
+//           </IconButton>
+//         }
+//         title="Fitness Tracker"
+//         subheader="September 14, 2016"
+//       />
+//       {/* <CardMedia
+//         className={classes.media}
+//         image="../images/randomDemoImage.jpg"
+//         title="Fitness Tracker"
+//       /> */}
+//       <CardContent>
+//         <Typography variant="body2" color="textSecondary" component="p">
+//           This is a fitness app idea
+//         </Typography>
+//       </CardContent>
+//       <CardActions disableSpacing>
+//         <IconButton aria-label="add to favorites">
+//           <FavoriteIcon />
+//         </IconButton>
+//         <IconButton aria-label="share">
+//           <ShareIcon />
+//         </IconButton>
+//         <IconButton
+//           className={clsx(classes.expand, {
+//             [classes.expandOpen]: expanded
+//           })}
+//           onClick={handleExpandClick}
+//           aria-expanded={expanded}
+//           aria-label="show more"
+//         >
+//           <ExpandMoreIcon />
+//         </IconButton>
+//       </CardActions>
+//       <Collapse in={expanded} timeout="auto" unmountOnExit>
+//         <CardContent>
+//           <Typography paragraph>More About:</Typography>
+//           <Typography paragraph>
+//             More About this Idea - More About this Idea- More About this Idea
+//           </Typography>
+//           <Typography paragraph>
+//             More About this Idea - More About this Idea- More About this Idea
+//             More About this Idea - More About this Idea- More About this Idea
+//             More About this Idea - More About this Idea- More About this Idea
+//             More About this Idea - More About this Idea- More About this Idea
+//           </Typography>
+//         </CardContent>
+//       </Collapse>
+//     </Card>
+//   );
+// }
+
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: '90vw'
+    flexGrow: 1
   },
-  media: {
-    height: 0,
-    paddingTop: '56.25%' // 16:9
+  paper: {
+    height: 140,
+    width: 100
   },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)'
-  },
-  avatar: {
-    backgroundColor: red[500]
+  control: {
+    padding: theme.spacing(2)
   }
 }));
 
-export default function PostCard() {
+export default function SpacingGrid() {
+  const [spacing, setSpacing] = React.useState(2);
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const handleChange = (event) => {
+    setSpacing(Number(event.target.value));
   };
 
   return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="post" className={classes.avatar}>
-            P
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Fitness Tracker"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        className={classes.media}
-        image="../images/randomDemoImage.jpg"
-        title="Fitness Tracker"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This is a fitness app idea
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>More About:</Typography>
-          <Typography paragraph>
-            More About this Idea - More About this Idea- More About this Idea
-          </Typography>
-          <Typography paragraph>
-            More About this Idea - More About this Idea- More About this Idea
-            More About this Idea - More About this Idea- More About this Idea
-            More About this Idea - More About this Idea- More About this Idea
-            More About this Idea - More About this Idea- More About this Idea
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    <Grid container className={classes.root} spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={spacing}>
+          {[0, 1, 2].map((value) => (
+            <Grid key={value} item>
+              <Paper className={classes.paper} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.control}>
+          <Grid container>
+            <Grid item>
+              <FormLabel>spacing</FormLabel>
+              <RadioGroup
+                name="spacing"
+                aria-label="spacing"
+                value={spacing.toString()}
+                onChange={handleChange}
+                row
+              >
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+                  <FormControlLabel
+                    key={value}
+                    value={value.toString()}
+                    control={<Radio />}
+                    label={value.toString()}
+                  />
+                ))}
+              </RadioGroup>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }

f99df49256651a1bc20c5399a8d9a57fd771d05f
phillidp1989 <61989740+phillidp1989@users.noreply.github.com>
2020-09-02 19:07:04 +0000

    Deploying to gh-pages from  @ ff4f9feb79585e11d768741a12762f8c96abff05 🚀

---
+++ precache-manifest.fceb18c57a276d13691b01e541707089.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb7d787284ef0f516b2b0dd83755b67",
+    "url": "/App-Factory/index.html"
+  },
+  {
+    "revision": "b371a21352a80772ee05",
+    "url": "/App-Factory/static/css/main.1e2adf8c.chunk.css"
+  },
+  {
+    "revision": "8dc7b9a5f597b33c5809",
+    "url": "/App-Factory/static/js/2.219fba4f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/App-Factory/static/js/2.219fba4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b371a21352a80772ee05",
+    "url": "/App-Factory/static/js/main.d9f78eae.chunk.js"
+  },
+  {
+    "revision": "ec3d9ad4f1e65d9ac00f",
+    "url": "/App-Factory/static/js/runtime-main.caa438fa.js"
+  },
+  {
+    "revision": "cea6dc4b536fd22a4bcd6dd313f9a446",
+    "url": "/App-Factory/static/media/google-logo.cea6dc4b.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a148a225f08f9ccfc38ad5c1b0c902a4.js"
+  "/App-Factory/precache-manifest.fceb18c57a276d13691b01e541707089.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/App-Factory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

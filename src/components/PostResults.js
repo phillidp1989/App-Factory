036c9059afe249d35b06f0a303494c212a5164a0
Dragos Dragomir <dragomir_costin_dragos@yahoo.com>
2020-09-04 14:48:23 +0100

    Update src/components/PostResults.js
    Co-authored-by: Dan Phillips <61989740+phillidp1989@users.noreply.github.com>

--- src/components/PostResults.js
+++ src/components/PostResults.js
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from 'react';
 import PostCard from './PostCard';
 import postData from './dummyData';
 import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}))
 export default function PostResults() {
   const [posts, setPost] = useState([]);
   useEffect(() => {

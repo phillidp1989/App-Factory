4e55bbe599889209ee0f880d619af6126224c44e
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-09-04 17:17:46 +0100

    revert PostResults.js to previous. No makeStyles

--- src/components/PostResults.js
+++ src/components/PostResults.js
@@ -2,14 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PostCard from './PostCard';
 import postData from './dummyData';
 import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center'
-  }
-}))
 export default function PostResults() {
   const classes = useStyles();
   const [posts, setPost] = useState([]);

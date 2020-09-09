611577158d1724fe9fa12455c85d0d3c314531b5
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-09-09 19:58:59 +0100

    API folder to handle api routes

---
+++ src/utils/API.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export default {
+  allPosts: async () => {
+    try {
+      return await axios.get('/api/posts')
+    } catch (err) {
+      console.error('ERROR - API.js - allPosts', err);
+    }
+  },
+  likePost: async (postId, userId) => {
+    try {
+      return await axios.put('/api/posts/like', {
+        postId,
+        userId
+      })
+    } catch (err) {
+      console.error('ERROR - API.js - allPosts', err);
+    }
+  },
+  unlikePost: async (postId, userId) => {
+    try {
+      return await axios.put('/api/posts/unlike', {
+        postId,
+        userId
+      })
+    } catch (err) {
+      console.error('ERROR - API.js - unlikePost', err);
+    }
+  }
+}

b634feada96dd55542638f9d6a783cafc05c2438
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-09-10 13:35:03 +0100

    useContext(UserContext) in userPosts.js

--- src/components/userPosts.js
+++ src/components/userPosts.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import PostCard from './PostCard';
 import axios from 'axios';
 import { Grid } from '@material-ui/core';
+import { UserContext } from '../context/UserContext';
 
 export default function UserPosts() {
   const [posts, setPosts] = useState([]);
+  const { user, isLoaded } = useContext(UserContext);
 
   useEffect(() => {
     const getAllPosts = async () => {
       try {
-        const { data } = await axios.get(
-          'https://app-factory-api.herokuapp.com/api/posts/user/5f458293eb85b45f1875891d'
-          // 'https://app-factory-djd.herokuapp.com/api/posts/user/5f458293eb85b45f1875891d'
-        );
+        if (user) {
+          const { data } = await axios.get(
+            `https://app-factory-api.herokuapp.com/api/posts/user/${user}`
+            // 'https://app-factory-djd.herokuapp.com/api/posts/user/5f458293eb85b45f1875891d'
+          );
 
-        setPosts(data);
+          setPosts(data);
+        }
       } catch (err) {
         console.error('ERROR - UserPosts() - getAllPosts', err);
       }
     };
     getAllPosts();
-  }, []);
+  }, [isLoaded]);
 
   return (
     <React.Fragment>

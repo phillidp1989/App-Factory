55181af870536b5b61351bbb16772d0aad90a35e
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-09-11 21:45:44 +0100

    brought in UserContext in UserSolutionsResults.js

--- src/components/UserSolutionsResults.js
+++ src/components/UserSolutionsResults.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import UserSolution from './UserSolution';
 import { Grid } from '@material-ui/core';
 import API from '../utils/API';
+import { UserContext } from '../context/UserContext';
 
 export default function UserSolutionsResults() {
   const [solutions, setSolutions] = useState([]);
+  const { user, isLoaded } = useContext(UserContext);
 
   useEffect(() => {
     const getUserSolutions = async () => {
       try {
-        const { data } = await API.getUserSolutions();
-        setSolutions(data.solutions);
-        console.log(data.solutions);
+        const { data } = await API.getUserSolutions(user._id);
+        setSolutions(data);
+        console.log(data);
       } catch (err) {
         console.error('ERROR - UserPosts() - getUserSolutions', err);
       }

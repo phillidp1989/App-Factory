6523a91a5e543b00faba51147fdb44809bf7f34d
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-09-11 21:45:58 +0100

    Added theming

--- src/App.js
+++ src/App.js
@@ -4,24 +4,40 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AllPosts from './pages/AllPosts';
 import Dashboard from './pages/Dashboard';
 import NewPost from './pages/NewPost';
+import AppPost from './pages/AppPost';
 import Login from './pages/Login';
 import PrivateRoute from './hocs/PrivateRoute';
 import { CssBaseline } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import GuestRoute from './hocs/GuestRoute';
+import { blue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: blue
+  }
+});
 
 export default function App() {
   return (
     <div className="App">
-      <CssBaseline />
-      <Router>
-        <Switch>
-          <GuestRoute path="/login" component={Login} />
-          <Route path="*" component={AppBar} />
-        </Switch>
-        <Route exact path="/" component={AllPosts} />
-        <PrivateRoute path="/dashboard" component={Dashboard} />
-        <PrivateRoute path="/posts/new" component={NewPost} />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Switch>
+            <GuestRoute path="/login" component={Login} />
+            <Route path="*" component={AppBar} />
+          </Switch>
+          <Route exact path="/" component={AllPosts} />
+          <PrivateRoute path="/dashboard" component={Dashboard} />
+          <Switch>
+            <PrivateRoute path="/posts/new" component={NewPost} />
+            <PrivateRoute path="/posts/edit/:id" component={NewPost} />
+            <Route path="/posts/:id" component={AppPost} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
